0b76df8549a51ece2486dcdf0c719c506d6099aa
Daksh Phalswal <dakshphalswalofficial@gmail.com>
2026-09-22 22:28:53 +0530

    Add files via upload

---
+++ Day 4/Indices_of_two_elements.c
@@ -0,0 +1,25 @@
+//Given an integer array arr and a target value target, find the indices of two elements whose
+//sum equals target. Assume exactly one valid pair exists, and the same element cannot be used twice.
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {2, 7, 11, 15};
+    int target = 9;
+    int n = 4;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] + arr[j] == target)
+            {
+                printf("[%d, %d]", i, j);
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
