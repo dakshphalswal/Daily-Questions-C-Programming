8b56e37291692aa1eadc62a345baa4ed6aa84b4e
Daksh Phalswal <dakshphalswalofficial@gmail.com>
2026-09-22 22:56:59 +0530

    Add files via upload

---
+++ Day 11/Square_elements.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[] = {-4, -3, -5, 1, 2, 3};
+    int n = 6;
+
+    int result[6];
+    int i, j, temp;
+
+    // Square each element
+    for (i = 0; i < n; i++)
+    {
+        result[i] = a[i] * a[i];
+    }
+
+    // Sort the squared values
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (result[i] > result[j])
+            {
+                temp = result[i];
+                result[i] = result[j];
+                result[j] = temp;
+            }
+        }
+    }
+
+    printf("Output: ");
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", result[i]);
+    }
+
+    return 0;
+}
